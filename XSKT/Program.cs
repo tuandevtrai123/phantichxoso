fba5bc0515a71c77860b51d061b33701b41260f2
tundevtrai123 <giatunn123@gmail.com>
2022-07-13 10:46:17 +0700

    first commit

---
+++ XSKT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XSKT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
